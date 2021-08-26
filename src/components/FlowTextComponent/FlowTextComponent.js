7c12c0a59797c2a53f4951f0ec929a9e34975cef
Manuel G <manuel.geissberger@gmail.com>
2021-08-26 23:12:30 +0200

    [EDIT] css vita improved

--- src/components/FlowTextComponent/FlowTextComponent.js
+++ src/components/FlowTextComponent/FlowTextComponent.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import styles from './FlowTextComponent.module.css';
 
-const componentStyle = {
-  p: {
-    margin: '20px',
-    fontSize: '1.2rem'
-  },
-  h1: {
-    margin: '20px',
-    marginTop: '40px',
-    marginBottom: '60px',
-  },
-};
-
 function FlowTextComponent(props) {
-  return props.flowText.isTitle ? <h1 style={componentStyle.h1}>{props.flowText.text}</h1> : <p style={componentStyle.p}>{props.flowText.text}</p>;
+  return props.flowText.isTitle ? <h1 className={styles.titleLine}>{props.flowText.text}</h1> : <p className={styles.paragraphLine}>{props.flowText.text}</p>;
 }
 
 export default FlowTextComponent;
