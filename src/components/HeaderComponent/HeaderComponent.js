b3e2c071c54b6cfcedd3ff655143d5bbf1c2dbbf
Manuel G <manuel.geissberger@gmail.com>
2021-08-26 20:09:11 +0200

    [EDIT] styling

--- src/components/HeaderComponent/HeaderComponent.js
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './HeaderComponent.module.css';
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { RouteDto } from './route';
 
-const HeaderComponent = () => (
-  <div className={styles.HeaderComponent} data-testid="HeaderComponent">    
-    <Link to="/">Home</Link>
-    <Link to="/vita">Vita</Link>
-    <Link to="/agenda">Agenda</Link>
-    <Link to="/fotos">Fotos</Link>
-    <Link to="/audio">Audio</Link>
-    <Link to="/press">Pressestimmen</Link>
-    <Link to="/repertoire">Repertoire</Link>
-    <Link to="/lessons">Unterricht</Link>
-    <Link to="/contact">Kontakt</Link>
+const routes = [
+  new RouteDto('/', "Home"),
+  new RouteDto('/vita', "Vita"),
+  new RouteDto('/agenda', "Agenda"),
+  new RouteDto('/fotos', "Fotos"),
+  new RouteDto('/audio', "Audio"),
+  new RouteDto('/press', "Pressestimmen"),
+  new RouteDto('/repertoire', "Repetoire"),
+  new RouteDto('/contact', "Kontakt")
+]
+
+function getStyle(route, actualRoute) {
+  if(route === actualRoute) return styles.selected
+  return styles.notSelected;
+}
+
+function HeaderComponent() {
+  const [actualRoute, setActualRoute ] = useState('/');
+
+
+
+  return <div className={styles.HeaderComponent} data-testid="HeaderComponent">  
+    {routes.map(route => <Link to={route.route} className={getStyle(route.route, actualRoute)} onClick={() => setActualRoute(route.route)}>{route.linkName}</Link>)}
   </div>
-);
-
-HeaderComponent.propTypes = {};
-
-HeaderComponent.defaultProps = {};
+}
 
 export default HeaderComponent;
---
+++ src/components/HeaderComponent/route.js
@@ -0,0 +1,6 @@
+export class RouteDto {
+    constructor(route, linkName) {
+        this.route = route;
+        this.linkName = linkName;
+    }
+}
