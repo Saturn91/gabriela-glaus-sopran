a3e37f676a7c56959c43e7dbd8b71eeb9100a66a
Manuel G <manuel.geissberger@gmail.com>
2021-08-26 16:49:47 +0200

    [EDIT] styling the project view

--- src/components/AgendaComponent/AgendaComponent.js
+++ src/components/AgendaComponent/AgendaComponent.js
@@ -5,9 +5,11 @@ import ProjectView from '../ProjectView/ProjectView';
 
 function AgendaComponent(){
   return <div className={styles.AgendaComponent} data-testid="AgendaComponent">
-    <KonzertView></KonzertView>
-    <div className={styles.spacer}></div>
-    <ProjectView></ProjectView> 
+    <div>
+      <KonzertView></KonzertView>
+      <div className={styles.spacer}></div>
+      <ProjectView></ProjectView> 
+    </div>    
   </div>
 }
 
--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -7,7 +7,7 @@ function ProjectView() {
 
   return <div className={styles.ProjectView} data-testid="ProjectView">
     <div className={styles.viewSelectorContainer}>
-      <h1 className={['title', styles.spacerTop].join(' ')}>Aktuelle Projekte</h1>
+      <h1 className={['title', styles.spacerTop].join(' ')}>Projekte</h1>
       <select name="cars" id="cars" className={styles.selector}>
         <option onClick={() => setDisplayState('actual')}>aktuelle</option>
         <option onClick={() => setDisplayState('past')}>vergangene</option>
