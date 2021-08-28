7c9f391e2156d0def6167ddf73f23713169fc174
Manuel G <manuel.geissberger@gmail.com>
2021-08-28 11:12:33 +0200

    [EDIT] improving css

--- src/components/AgendaComponent/AgendaComponent.js
+++ src/components/AgendaComponent/AgendaComponent.js
@@ -6,9 +6,7 @@ import ProjectView from '../ProjectView/ProjectView';
 function AgendaComponent(){
   return <div className={styles.AgendaComponent} data-testid="AgendaComponent">
     <div>
-      <KonzertView></KonzertView>
-      <div className={styles.spacer}></div>
-      <ProjectView></ProjectView> 
+      
     </div>    
   </div>
 }
