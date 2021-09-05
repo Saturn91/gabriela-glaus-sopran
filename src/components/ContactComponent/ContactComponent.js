2ecc26854fe4e08d64c89f0b61ae4360da59b16a
Manuel G <manuel.geissberger@gmail.com>
2021-09-05 14:08:51 +0200

    [EDIT] Contactcomponent email form styled

--- src/components/ContactComponent/ContactComponent.js
+++ src/components/ContactComponent/ContactComponent.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from './ContactComponent.module.css';
+import mainStyles from '../mainStyle.module.css';
+import { getVitaImage } from '../../services/image.service';
 
-const ContactComponent = () => (
-  <div className={styles.ContactComponent} data-testid="ContactComponent">
-    ContactComponent Component
+function ContactComponent(){  
+
+  return <div className={mainStyles.componentMainStyle} data-testid="ContactComponent">
+    <div className={[mainStyles.mainDivHorizontalCenteredChildren]}>
+      {getVitaImage([mainStyles.backgroundImage, mainStyles.hideOnPhones].join(' '))}
+      <div className={[mainStyles.mainCenteredInlayDiv, styles.ContactComponent].join(' ')}>
+      <form className={styles.emailForm}>
+        <input type="Email" placeholder="maximilian.muster@hotmail.com"></input>
+        <input type="Text" placeholder="Dein Betreff"></input>
+        <textarea placeholder="Deine Nachricht" rows="10"></textarea>
+        <div className={styles.buttonHolder}>
+          <button className={styles.left}>Submit</button>
+          <button className={styles.right}>Cancel</button>
+        </div>        
+      </form>  
+      </div>    
+    </div>      
   </div>
-);
+}
 
 export default ContactComponent;
