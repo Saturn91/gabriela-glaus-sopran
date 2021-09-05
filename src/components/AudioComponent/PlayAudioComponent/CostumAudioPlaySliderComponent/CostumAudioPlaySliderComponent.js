ba0adbfbab4fdb66d051ffbc1c8b00bdc275bfe4
Manuel G <manuel.geissberger@gmail.com>
2021-09-05 13:15:36 +0200

    [EDIT] costum play asset is ready and responsive

--- src/components/AudioComponent/PlayAudioComponent/CostumAudioPlaySliderComponent/CostumAudioPlaySliderComponent.js
+++ src/components/AudioComponent/PlayAudioComponent/CostumAudioPlaySliderComponent/CostumAudioPlaySliderComponent.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import styles from './CostumAudioPlaySliderComponent.module.css';
 
-import playBtn from '../../../../icons/play.png'
-import volumeIcon from '../../../../icons/volume.png'
+import playBtn from '../../../../icons/play.png';
+import volumeIcon from '../../../../icons/volume.png';
+import mutedVolumeIcon from '../../../../icons/volume_mute.png';
 
 let currentSong; 
 
@@ -19,6 +20,23 @@ function playAudio(audio) {
   } else {
     stopAudio(audio); 
   } 
+}
+
+function setAudioVolume(audio, volume, callBack, flag) {
+  let audioVolume = volume / 100;
+  if(volume > 95) audioVolume = 1;
+  if(volume < 10) audioVolume = 0;
+  callBack(audioVolume, flag);
+  audio.volume = audioVolume;
+}
+
+function toggleMute(audio, callback, flag) {
+    if(audio.volume > 0.2) {
+      audio.volume = 0;
+    } else {
+      audio.volume = 0.5;
+    }
+    callback(audio.volume, flag);
 }
 
 function formatDuration(duration) {
@@ -42,16 +60,26 @@ function CostumAudioPlaySliderComponent(audioUrl) {
   const [volume, setVolume] = useState(50);  
   const [endPosition, setEndPosition] = useState(0.1);
   const [actualPosition, setActualPosition] = useState(1);
+  const [actualVolumeIcon, setActualVolumeIcon] = useState(volumeIcon);
 
   useEffect(() => {
     audio.oncanplaythrough = () => {
       setEndPosition(audio.duration);
       audio.volume = volume / 100;
-      setVolume(audio.volume*100);   
+      setVolume(audio.volume*100);
     }    
   });
 
-  useEffect(() => audio.ontimeupdate = () => setActualPosition(audio.currentTime / endPosition * 10000), [endPosition, audio])
+  useEffect(() => audio.ontimeupdate = () => setActualPosition(audio.currentTime / endPosition * 10000), [endPosition, audio]);
+
+  function updateVolumeIcon(value, flagSetVolume) {
+    if (value >= 0.2) {
+      setActualVolumeIcon(volumeIcon);
+    } else {
+      setActualVolumeIcon(mutedVolumeIcon);
+    }
+    if(flagSetVolume) setVolume(value*100);
+  }
 
   return <div className={styles.CostumAudioPlaySliderComponent} data-testid="CostumAudioPlaySliderComponent">
     <img className={styles.icon} src={playBtn} alt='play' onClick={() => playAudio(audio)} style={{pointerEvents: "all"}}></img>
@@ -72,14 +100,11 @@ function CostumAudioPlaySliderComponent(audioUrl) {
         <p className={styles.actualPosition}>{formatDuration((actualPosition-1) / 10000 * endPosition)}</p>
         <p className={styles.endPosition}>{"/ " +formatDuration(endPosition)}</p>
     </div>
-    <img className={styles.icon} src={volumeIcon} alt='volume'/>
+    <img className={styles.icon} src={actualVolumeIcon} alt='volume' onClick={() => toggleMute(audio, updateVolumeIcon, true)} style={{pointerEvents: "all"}}/>
     <div className={styles.volumeSliderContainer}>
         <input type="range" min="1" max="100" value={volume} onInput={() => {
           setVolume(document.getElementById(volumeSliderID).value);
-          let audioVolume = Math.log(volume/10+1)/Math.log(10);
-          if(audioVolume > 1) audioVolume = 1;
-          if(audioVolume < 0.2) audioVolume = 0;
-          audio.volume = audioVolume;
+          setAudioVolume(audio, volume, updateVolumeIcon, false);
         }} 
         className={styles.volumeSlider} 
         id={volumeSliderID}/>
