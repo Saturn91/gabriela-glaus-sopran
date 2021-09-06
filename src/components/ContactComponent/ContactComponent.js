bc9bd6d8c236d018cd3a491dbc3e5d2026797246
Manuel G <manuel.geissberger@gmail.com>
2021-09-06 10:49:06 +0200

    [ADD] Submit and cancel action in Contact Component

--- src/components/ContactComponent/ContactComponent.js
+++ src/components/ContactComponent/ContactComponent.js
@@ -2,20 +2,32 @@ import React from 'react';
 import styles from './ContactComponent.module.css';
 import mainStyles from '../mainStyle.module.css';
 import { getVitaImage } from '../../services/image.service';
+import { sendEmail } from '../../services/email.service';
+
+const formId = 'contactForm';
+
+function onFormSubmit(onSubmit) {
+  onSubmit.preventDefault();
+  sendEmail(onSubmit.target[0].value, onSubmit.target[1].value, onSubmit.target[2].value);
+  clearForm();
+}
+
+function clearForm() {
+  document.getElementById(formId).reset();
+}
 
 function ContactComponent(){  
-
   return <div className={mainStyles.componentMainStyle} data-testid="ContactComponent">
     <div className={[mainStyles.mainDivHorizontalCenteredChildren]}>
       {getVitaImage([mainStyles.backgroundImage, mainStyles.hideOnPhones].join(' '))}
       <div className={[mainStyles.mainCenteredInlayDiv, styles.ContactComponent].join(' ')}>
-      <form className={styles.emailForm}>
-        <input type="Email" placeholder="maximilian.muster@hotmail.com"></input>
-        <input type="Text" placeholder="Dein Betreff"></input>
-        <textarea placeholder="Deine Nachricht" rows="10"></textarea>
+      <form className={styles.emailForm} onSubmit={(onSumbit) => onFormSubmit(onSumbit)} id={formId}>
+        <input type="Email" placeholder="maximilian.muster@hotmail.com" name='email' required></input>
+        <input type="Text" placeholder="Dein Betreff" name='subject' required></input>
+        <textarea placeholder="Deine Nachricht" rows="10" name='message' required></textarea>
         <div className={styles.buttonHolder}>
-          <button className={styles.left}>Submit</button>
-          <button className={styles.right}>Cancel</button>
+          <button className={styles.left} type='submit'>Submit</button>
+          <button className={styles.right} type='button' onClick={() => clearForm()}>Cancel</button>
         </div>        
       </form>  
       </div>    
---
+++ src/services/email.service.js
@@ -0,0 +1,3 @@
+export function sendEmail(emailAdress, subject, message) {
+    alert('email was not send as this is still a test build: from ' + emailAdress + ': ' + subject + ', msg: ' + message);
+}
