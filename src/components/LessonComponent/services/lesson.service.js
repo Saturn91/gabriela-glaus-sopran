f68fe71b2fe052b86a77e3c9379ade5efb3e8a8a
Manuel G <manuel.geissberger@gmail.com>
2021-09-25 10:54:50 +0200

    [BUG-FIX] other flow texts did not have text pos

--- src/components/LessonComponent/services/lesson.service.js
+++ src/components/LessonComponent/services/lesson.service.js
@@ -5,19 +5,19 @@ import { FlowTextParagraph, FlowTextType } from "../../../models/flowtext/flowte
 
 const lessons = [
     new ImagePost(new ImageDTO("lesson_Image", "Description", "https://i.postimg.cc/NjWtfWyf/Lesson-Banner.jpg"), new FlowText([
-        new FlowTextParagraph(FlowTextType.TITLE, "Musikschulen"),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "Ich unterrichte an der Musikschule in Gersau und in Schwarzenberg Gesang und den Kinderchor."),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "Möchten Sie sich selbst oder Ihr Kind für den Gesangsunterricht anmelden, zögern Sie nicht, mich zu kontaktieren. Ich freue mich auf Sie!"),
-        new FlowTextParagraph(FlowTextType.LINK, "Website Musikschule Gersau", "https://www.bezirksschule-gersau.ch/musikschule/"),
-        new FlowTextParagraph(FlowTextType.LINK, "Website Musikschule Schwarzenberg","https://schwarzenberg.ch/schule/musikschule"),
+        new FlowTextParagraph(0, FlowTextType.TITLE, "Musikschulen"),
+        new FlowTextParagraph(1, FlowTextType.PARAGRAPH, "Ich unterrichte an der Musikschule in Gersau und in Schwarzenberg Gesang und den Kinderchor."),
+        new FlowTextParagraph(2, FlowTextType.PARAGRAPH, "Möchten Sie sich selbst oder Ihr Kind für den Gesangsunterricht anmelden, zögern Sie nicht, mich zu kontaktieren. Ich freue mich auf Sie!"),
+        new FlowTextParagraph(3, FlowTextType.LINK, "Website Musikschule Gersau", "https://www.bezirksschule-gersau.ch/musikschule/"),
+        new FlowTextParagraph(4, FlowTextType.LINK, "Website Musikschule Schwarzenberg","https://schwarzenberg.ch/schule/musikschule"),
     ],
     )),
     new ImagePost(new ImageDTO("lesson_Image", "Description", "https://i.postimg.cc/NjWtfWyf/Lesson-Banner.jpg"), new FlowText([
-        new FlowTextParagraph(FlowTextType.TITLE, "Gesangsunterricht"),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "„Singen ist das Fundament zur Musik in allen Dingen.“ - (Georg Philipp Telemann)"),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "Das schönste am Singen ist, dass man mit der eigenen Stimme Gefühle ausdrücken und andere Menschen berühren kann. Im Unterricht lernt man die eigene Stimme kennen und entwickelt sie weiter."),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "Wenn Sie Lust haben, singen zu lernen, zögern Sie nicht, mich über das Kontaktformular anzuschreiben."),
-        new FlowTextParagraph(FlowTextType.PARAGRAPH, "Ich freue mich darauf, viele singbegeisterte Menschen kennenzulernen, sie auf diesem Weg zu betreuen, anzuleiten und zu coachen.")
+        new FlowTextParagraph(0, FlowTextType.TITLE, "Gesangsunterricht"),
+        new FlowTextParagraph(1, FlowTextType.PARAGRAPH, "„Singen ist das Fundament zur Musik in allen Dingen.“ - (Georg Philipp Telemann)"),
+        new FlowTextParagraph(2, FlowTextType.PARAGRAPH, "Das schönste am Singen ist, dass man mit der eigenen Stimme Gefühle ausdrücken und andere Menschen berühren kann. Im Unterricht lernt man die eigene Stimme kennen und entwickelt sie weiter."),
+        new FlowTextParagraph(3, FlowTextType.PARAGRAPH, "Wenn Sie Lust haben, singen zu lernen, zögern Sie nicht, mich über das Kontaktformular anzuschreiben."),
+        new FlowTextParagraph(4, FlowTextType.PARAGRAPH, "Ich freue mich darauf, viele singbegeisterte Menschen kennenzulernen, sie auf diesem Weg zu betreuen, anzuleiten und zu coachen.")
     ],
     )),
 ];
--- src/services/pressentry.service.js
+++ src/services/pressentry.service.js
@@ -8,9 +8,9 @@ import mrChoufleriePDF from "../test_docs/Monsieur-Choufleuri-Oberbaselbieter-Ze
 const pressEntries = [
     new PressEntry(
         new FlowText([
-            new FlowTextParagraph(FlowTextType.TITLE, "Stell dir vor, es ist Tag der offenen Tür, und…"),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, '„Die öffentliche Probe des Ensembles Operadieschen gab einen Einblick in die hochkonzentrierte Arbeit eines freien Ensembles. Gabriela Glaus (Sopran), Julia Zeier (Mezzosopran), Timothy Löw (Tenor), Maxence Douez (Tenor), Tobias Wurmehl (Bassbariton), Sandra Hamburger (Klavier) und Christian Kipper (Regie) meisterten die kleine Operette mit Bravour…'),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Nach fünfviertel Stunden hatte man nicht nur einen Einblick in die Operette, sondern auch in die Knochenarbeit eines jungen freien Ensembles, das neben seiner Professionalität auch immer noch viel Spass durchscheinen liess.“'),
+            new FlowTextParagraph(0, FlowTextType.TITLE, "Stell dir vor, es ist Tag der offenen Tür, und…"),
+            new FlowTextParagraph(1, FlowTextType.PARAGRAPH, '„Die öffentliche Probe des Ensembles Operadieschen gab einen Einblick in die hochkonzentrierte Arbeit eines freien Ensembles. Gabriela Glaus (Sopran), Julia Zeier (Mezzosopran), Timothy Löw (Tenor), Maxence Douez (Tenor), Tobias Wurmehl (Bassbariton), Sandra Hamburger (Klavier) und Christian Kipper (Regie) meisterten die kleine Operette mit Bravour…'),
+            new FlowTextParagraph(2, FlowTextType.PARAGRAPH, 'Nach fünfviertel Stunden hatte man nicht nur einen Einblick in die Operette, sondern auch in die Knochenarbeit eines jungen freien Ensembles, das neben seiner Professionalität auch immer noch viel Spass durchscheinen liess.“'),
         ]),
         new Date('2020-03-19'),
         undefined,
@@ -19,10 +19,10 @@ const pressEntries = [
     ),
     new PressEntry(
         new FlowText([
-            new FlowTextParagraph(FlowTextType.TITLE, "Und selbst die Pfarrkirche St. Marzellus hat eifrig mitvibriert"),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Es basiert sicher auf der aussergewöhnlichen Fähigkeit von Gabriela Glaus zu spüren, was mit dem Chor und dem Orchester erreicht werden kann und die Mitglieder entsprechend zu motivieren und zu leiten.'),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Ganzer Artikel, Konzert Mission (Im)Possible, chor cantando Gersau und Juventus Musica Basel'),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Walter Jann, Wochenzeitung Gersau'),
+            new FlowTextParagraph(0, FlowTextType.TITLE, "Und selbst die Pfarrkirche St. Marzellus hat eifrig mitvibriert"),
+            new FlowTextParagraph(1, FlowTextType.PARAGRAPH, 'Es basiert sicher auf der aussergewöhnlichen Fähigkeit von Gabriela Glaus zu spüren, was mit dem Chor und dem Orchester erreicht werden kann und die Mitglieder entsprechend zu motivieren und zu leiten.'),
+            new FlowTextParagraph(2, FlowTextType.PARAGRAPH, 'Ganzer Artikel, Konzert Mission (Im)Possible, chor cantando Gersau und Juventus Musica Basel'),
+            new FlowTextParagraph(3, FlowTextType.PARAGRAPH, 'Walter Jann, Wochenzeitung Gersau'),
         ]),
         new Date('2019-10-31'),
         "https://www.gabrielaglaus-sopran.ch/wp-content/uploads/Zeitungsberichte-Gersau.pdf",
@@ -31,12 +31,12 @@ const pressEntries = [
     ),
     new PressEntry(
         new FlowText([
-            new FlowTextParagraph(FlowTextType.TITLE, "Ein Titel"),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Erster Paragraph'),
-            new FlowTextParagraph(FlowTextType.TITLE, "Ein zweiter Titel"),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Zweiter Pargraph'),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Bild, und Link sind absolut optional, du kannst entscheiden ob du ein PDF oder einen externen Link unter "mehr" öffnen willst'),
-            new FlowTextParagraph(FlowTextType.PARAGRAPH, 'Geolino: Manuel Geissberger, Autoren Zeile kannst du gestalten wie du möchtest'),
+            new FlowTextParagraph(0, FlowTextType.TITLE, "Ein Titel"),
+            new FlowTextParagraph(1, FlowTextType.PARAGRAPH, 'Erster Paragraph'),
+            new FlowTextParagraph(2, FlowTextType.TITLE, "Ein zweiter Titel"),
+            new FlowTextParagraph(3, FlowTextType.PARAGRAPH, 'Zweiter Pargraph'),
+            new FlowTextParagraph(4, FlowTextType.PARAGRAPH, 'Bild, und Link sind absolut optional, du kannst entscheiden ob du ein PDF oder einen externen Link unter "mehr" öffnen willst'),
+            new FlowTextParagraph(5, FlowTextType.PARAGRAPH, 'Geolino: Manuel Geissberger, Autoren Zeile kannst du gestalten wie du möchtest'),
         ]),
         new Date('2021-09-06'),
         undefined,
